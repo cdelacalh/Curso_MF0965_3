5591389f13bcfffab78da8e153f46b72c73875fd
cdelacalh <cdelacalh@gmail.com>
2024-09-06 10:04:15 +0200

    inicio del repositorio

--- Ejercicios_01/src/main/java/com/corenetworks/presentacion/Main.java
+++ Ejercicios_01/src/main/java/com/corenetworks/presentacion/Main.java
@@ -1,7 +1,9 @@
-package com.corenetworks;
+package com.corenetworks.presentacion;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("Hola mundo");
     }
 }
